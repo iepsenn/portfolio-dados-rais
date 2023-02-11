a033dc73bba10cdc63c9eae5163797463bcbfa46
iepsenn <lukasiepsen@gmail.com>
2023-02-11 18:42:53 -0300

    refactor: remove unnecessary files

--- dags/extract_data_from_ftp_to_s3.py
+++ dags/extract_data_from_ftp_to_s3.py
@@ -1,36 +1,45 @@
-from email import policy
 import pendulum
 from airflow.decorators import task, dag
 from airflow.providers.amazon.aws.transfers.ftp_to_s3 import FTPToS3Operator
-from airflow.providers.amazon.aws.transfers.local_to_s3 import  LocalFilesystemToS3Operator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+
+months = [
+    str(number)
+    if number >= 10
+    else f'0{number}'
+    for number in range(1, 13)
+]
+
 
 @dag(
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    schedule_interval="@hourly",
+    dag_id='extract_data_from_ftp_to_s3',
+    start_date=pendulum.datetime(2020, 1, 1, tz="UTC"),
+    schedule_interval="@yearly",
     max_active_runs=1,
     catchup=False
 )
 def extract_data_from_ftp_to_s3():
-    ftp_to_s3_task = FTPToS3Operator(
-        task_id="ftp_to_s3_task",
-        aws_conn_id="local_minio",
-        ftp_path="/pdet/microdados/NOVO CAGED/2020/202002/",
-        s3_bucket='caged',
-        s3_key='raw/2022B',
-        replace=True,
-        acl_policy="owner"
+    year = '{{ data_interval_start.year }}'
+
+    create_bucket = S3CreateBucketOperator(
+        aws_conn_id='local_minio',
+        task_id="create_bucket",
+        bucket_name='caged'
     )
 
-#def LocalToS3Operator():
-#    local_to_s3_task =  LocalFilesystemToS3Operator(
-#        task_id="local_to_s3_task",
-#        aws_conn_id="s3_conn",
-#        filename = "/opt/airflow/test.txt",
-#        dest_bucket='minio',
-#        dest_key='caged/a.7z',
-#        replace=True,
-#        acl_policy="owner"
-#    )
+    for month in months[:1]:
+        ftp_to_s3_task = FTPToS3Operator(
+            task_id=f"extract_{year}{month}_files_to_s3",
+            aws_conn_id="local_minio",
+            ftp_path=f"/pdet/microdados/NOVO CAGED/{year}/{year}{month}",
+            ftp_filenames='*',
+            s3_bucket='caged',
+            s3_key=f'raw/{year}{month}/',
+            replace=True,
+            acl_policy="owner"
+        )
+        create_bucket >> ftp_to_s3_task
+
 
 dag = extract_data_from_ftp_to_s3()
-#dag = LocalToS3Operator()
--- ftp.py
+++
@@ -1,9 +0,0 @@
-from ftplib import FTP
-ftp = FTP('ftp.mtps.gov.br')
-ftp.login()
-
-ftp.retrlines('LIST') # ls
-ftp.cwd('pdet') # cd
-
-ftp.nlst("microdados") # return a list python of files and dirs
-
